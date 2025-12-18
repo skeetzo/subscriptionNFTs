587af8bffbe866cdb0f1c643d3dcbfd0283740c4
Skeetzo <WebmasterSkeetzo@gmail.com>
2025-12-18 11:26:16 -0800

    0.0.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,8 +2,10 @@
 
 **0.0.0 : 12-17-2025**
 - init
-
+**0.0.1 : 12-18-2025**
+- finished ERC-948 tests
 
 # TODO
 
-- finish ERC-948 tests
+- add contract interface for ERC-948
+- create missing contracts from interfaces
--- test/ERC948.test.js
+++ test/ERC948.test.js
@@ -1,22 +1,26 @@
 import { expect } from "chai";
 import { network } from "hardhat";
-import { anyValue } from "@nomicfoundation/hardhat-ethers-chai-matchers/withArgs";
+// import { anyValue } from "@nomicfoundation/hardhat-ethers-chai-matchers/withArgs";
 const { ethers, networkHelpers } = await network.connect();
 
-const expires = Date.now()+(60*60*24*1);
-const tokenId = 0;
-const duration = 100;
+const amountRecurring = 1, 
+      amountInitial = 1, 
+      periodType = 0, 
+      periodMultiplier = 1, 
+      startTime = Date.now()+(60*60*24*1), 
+      data = "";
 
-const amountRecurring = 1, amountInitial = 1, periodType = 0, periodMultiplier = 1, startTime = Date.now()+(60*60*24*1), data = "";
-const amount = 1;
-
-async function getTimestamp(tx) {
-  return (await ethers.provider.getBlock(tx.blockNumber)).timestamp;
+async function createSubscription(ERC20, ERC948, to, from) {
+  await ERC20.approve(ERC948.target, amountInitial+amountRecurring)
+  const tx = await ERC948.createSubscription(to, ERC20.target, amountRecurring, amountInitial, periodType, periodMultiplier, startTime, data);
+  const timestamp = (await ethers.provider.getBlock(tx.blockNumber)).timestamp;
+  // console.log("timestamp:", timestamp)
+  const subscriptionId = ethers.solidityPackedKeccak256(["address", "uint256"], [from, timestamp]);
+  // console.log("subscriptionId:", subscriptionId)
+  return [subscriptionId, tx];
 }
 
 describe("ERC-948 Contract", async function () {
-
-  let subscriptionId;
 
   async function deployFixture() {
     const [owner, addr1, addr2] = await ethers.getSigners();
@@ -30,49 +34,43 @@ describe("ERC-948 Contract", async function () {
 
     it("can create subscription", async function () {
       const { ERC948, ERC20, owner, addr1 } = await networkHelpers.loadFixture(deployFixture);
-      await ERC20.approve(addr1.address, amountInitial+amountRecurring)
-      const tx = await ERC948.createSubscription(addr1.address, ERC20.target, amountRecurring, amountInitial, periodType, periodMultiplier, startTime, data);
-      const timestamp = await getTimestamp(tx);
-      subscriptionId = ethers.utils.solidityPack(["address", "uint256"], [owner.address, timestamp])
+      const [subscriptionId, tx] = await createSubscription(ERC20, ERC948, addr1.address, owner.address);
       expect(tx).to.emit(ERC948, "NewSubscription").withArgs(subscriptionId, addr1.address, ERC20.target, amountRecurring, amountInitial, periodType, periodMultiplier, startTime);
     });
 
     it("can get subscribers subscriptions", async function () {
-      const { ERC948, owner, addr1 } = await networkHelpers.loadFixture(deployFixture);
-      const subscriptions = await ERC948.getSubscribersSubscriptions(owner.address);
-      // subscriptionId = 
-      // owner.address + timestamp
+      const { ERC948, ERC20, owner, addr1 } = await networkHelpers.loadFixture(deployFixture);
+      let subscriptions = await ERC948.getSubscribersSubscriptions(owner.address);
+      expect(subscriptions).to.deep.equal([]);
+      const [subscriptionId,] = await createSubscription(ERC20, ERC948, addr1.address, owner.address);
+      subscriptions = await ERC948.getSubscribersSubscriptions(owner.address);
       expect(subscriptions).to.deep.equal([subscriptionId]);
     });
 
-
     it("can cancel subscription", async function () {
-      const { ERC948, owner, addr1 } = await networkHelpers.loadFixture(deployFixture);
-      const canceled = await ERC948.cancelSubscription(subscriptionId)
+      const { ERC948, ERC20, owner, addr1 } = await networkHelpers.loadFixture(deployFixture);
+      const [subscriptionId,] = await createSubscription(ERC20, ERC948, addr1.address, owner.address);
+      const canceled = await ERC948.cancelSubscription.staticCall(subscriptionId);
       expect(canceled).to.be.true;
     });
 
     it("can get payment due", async function () {
-      const { ERC948, owner, addr1 } = await networkHelpers.loadFixture(deployFixture);
-      const isDue = await ERC948.paymentDue(subscriptionId);
+      const { ERC948, ERC20, owner, addr1 } = await networkHelpers.loadFixture(deployFixture);
+      const [subscriptionId,] = await createSubscription(ERC20, ERC948, addr1.address, owner.address);
+      let isDue = await ERC948.paymentDue(subscriptionId);
       expect(isDue).to.be.false;
-      // increase time
+      await networkHelpers.time.increase(periodMultiplier+1);
+      isDue = await ERC948.paymentDue(subscriptionId);
       expect(isDue).to.be.true;
     });
 
     it("can process subscription", async function () {
-      const { ERC948, owner, addr1 } = await networkHelpers.loadFixture(deployFixture);
-      const processed = await ERC948.processSubscription(subscriptionId, amount);
+      const { ERC948, ERC20, owner, addr1 } = await networkHelpers.loadFixture(deployFixture);
+      const [subscriptionId,] = await createSubscription(ERC20, ERC948, addr1.address, owner.address);
+      await networkHelpers.time.increase(periodMultiplier+1);
+      const processed = await ERC948.processSubscription.staticCall(subscriptionId, amountRecurring);
       expect(processed).to.be.true;
-
     });
-
-
-
-
-
-
-
 
   });
 
